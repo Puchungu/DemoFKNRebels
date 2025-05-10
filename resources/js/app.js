7aa5b0d4398f023aeb19adef8d89ab7005c9833f
Puchungu <nelsonalvarenga1000@gmail.com>
2025-05-09 22:56:13 -0600

    Add toggle functionality for menu and user dropdown

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+const toggleBtn = document.querySelector('.menu-toggle');
+const navList = document.querySelector('.list-container');
+
+const toggleBtn2 = document.querySelector('.user-toggle');
+const navList2 = document.querySelector('.user-dropdown');
+
+
+toggleBtn.addEventListener('click', () => {
+    navList.classList.toggle('show');
+});
+
+toggleBtn2.addEventListener('click', () => {
+    navList2.classList.toggle('show');
+});
+
+
