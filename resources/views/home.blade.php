ea4c88d815ab69e7ea90f4a4159d1b3f7ccb6f77
Puchungu <nelsonalvarenga1000@gmail.com>
2025-05-09 22:56:30 -0600

    Remove logout form from home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,9 @@
     <title>home</title>
 </head>
 <body>
+    @include('header')
     @auth
         <h2> Hi there, {{Auth::user()->username}}</h2>
-        <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button>Logout</button>
-        </form>
     @endauth
 </body>
 </html>
