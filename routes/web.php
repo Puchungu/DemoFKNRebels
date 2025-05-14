32f98e94c2013da0e528b20eb6e4655cba37ea30
Puchungu <nelsonalvarenga1000@gmail.com>
2025-05-14 00:04:43 -0600

    Add admin home route and update product routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/register',[AuthController::class, 'showRegister'])->name('show.register');
 Route::get('/login',[AuthController::class, 'showLogin'])->name('show.login');
 Route::post('/register',[AuthController::class, 'register'])->name('register');
 Route::post('/login',[AuthController::class, 'login'])->name('login');
 Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
+Route::get('/admin/home',[AuthController::class, 'showAdmin'])->name('admin.home');
 
 
-
+Route::get('/products',[ProductController::class, 'index'])->name('products');
+Route::get('/products',[ProductController::class, 'show'])->name('products');
 
 Route::get('/', function () {
     return view('home');
